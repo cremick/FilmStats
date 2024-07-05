8549f2bd82db1f0268449e9675c88917d88f55dc
github_id <github_email>
2024-07-05 09:53:41 -0400

    genre create endpoint

--- api/Controllers/FilmGenreController.cs
+++ api/Controllers/FilmGenreController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Interfaces;
 using api.Mappers;
+using api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -39,6 +40,43 @@ namespace api.Controllers
             var genreFilmDtos = genreFilms.Select(f => f.ToFilmDto()).ToList();
 
             return Ok(genreFilmDtos);
+        }
+
+        [HttpPost("{genreId:int}/films")]
+        public async Task<IActionResult> Create([FromRoute] int genreId, string title)
+        {
+            var genre = await _genreRepo.GetByIdAsync(genreId);
+            var film = await _filmRepo.GetByTitleAsync(title);
+
+            if (film == null) 
+                return BadRequest("Film not found");
+
+            if (genre == null)
+                return BadRequest("Genre not found");
+
+            var genreFilms = await _filmGenreRepo.GetGenreFilmsAsync(genre);
+
+            if (genreFilms.Any(f => f.Title.ToLower() == title.ToLower())) 
+                return BadRequest("Film already belongs to this genre");
+
+            var filmGenreModel = new FilmGenre
+            {
+                FilmId = film.Id,
+                GenreId = genre.Id,
+                Film = film,
+                Genre = genre
+            };
+
+            var createdFilmGenre = await _filmGenreRepo.CreateAsync(filmGenreModel);
+
+            if (createdFilmGenre == null)
+            {
+                return StatusCode(500, "Could not create");
+            }
+            else
+            {
+                return Created();
+            }
         }
     }
 }
