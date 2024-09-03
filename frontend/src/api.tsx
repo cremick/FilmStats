9c0381172800531ca38257f3d1e35e441f5529d2
github_id <github_email>
2024-09-03 16:54:56 -0400

    incorporating tmdb api

---
+++ frontend/src/api.tsx
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { FilmSearch } from "./film"
+
+interface SearchResponse {
+    data: FilmSearch[];
+}
+
+export const searchFilms = async (query: string) => {
+    try {
+        const data = await axios.get<SearchResponse>(
+            `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`
+        );
+        return data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.log("error message: ", error.message);
+        } else {
+            console.log("unexpected error: ", error);
+            return "An unexpected error has occured.";
+        }
+    }
+}
---
+++ frontend/src/film.d.ts
@@ -0,0 +1,8 @@
+export interface FilmSearch {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+    release_date: string;
+}
+  
